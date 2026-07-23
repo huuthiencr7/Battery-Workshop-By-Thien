7cb2f0fae7865be394322fa71f458c6bc9549df9
huuthiencr7 <huuthien03022000@gmail.com>
2026-07-23 11:57:43 +0700

    add in new product

--- data.js
+++ data.js
@@ -126,7 +126,7 @@ const products = [
         id: 20, 
         category: 'may-cong-cu', 
         name: 'Pin chân phổ thông 5Ah', 
-        fullDesc: 'Pin chân phổ thông 5Ah\n  Phân loại: 10 Cell 18650, báo pin phía trước\n Lõi Pin: EVE25P , Vtc5d, EVE35V, Sunpower...\n Mạch bảo vệ: BW-60A, BM-90A, DX-100A\n Chất liệu: Nhựa ABS\n Giá: 450-550k\n Liên hệ để được tư vấn giá, chọn lõi pin và mạch phù hợp', 
+        fullDesc: 'Pin chân phổ thông 5Ah\n  Phân loại: 10 Cell 18650, báo pin phía trước\n Lõi Pin: EVE25P , Vtc5d,Sunpower, EVE35V, EVE33v...\n Mạch bảo vệ: BW-60A, BM-90A, DX-100A\n Chất liệu: Nhựa ABS\n Giá: 450-550k\n Liên hệ để được tư vấn giá, chọn lõi pin và mạch phù hợp', 
         image: 'media/10_cell_PT_2.jpg',
         // Thêm mảng này để chứa nhiều hình cho phần xem chi tiết
         images: [
@@ -166,8 +166,22 @@ const products = [
     { 
         id: 23, 
         category: 'may-cong-cu', 
+        name: 'Pin chân phổ thông 8Ah', 
+        fullDesc: 'Pin chân phổ thông 8Ah\n Phân loại: 10 Cell 21700, báo pin phía trước\n Lõi Pin: EVE40P, Vtc6A, Samsung40T, LG50LT, Samsung50E...\n Mạch bảo vệ: BW-60A, BM-90A, DX-100A\n Chất liệu: Nhựa ABS\n Giá: 650-750K\n Liên hệ để được tư vấn giá, chọn lõi pin và mạch phù hợp', 
+        image: 'media/10_cell_PT_21700_2.jpg',
+        // Thêm mảng này để chứa nhiều hình cho phần xem chi tiết
+        images: [
+           "media/10_cell_PT_21700_2.jpg",  
+           "media/10_cell_PT_21700_1.jpg",
+           "media/10_cell_PT_21700.jpg"
+         
+        ]
+    },
+    { 
+        id: 24, 
+        category: 'may-cong-cu', 
         name: 'Pin chân phổ thông 12Ah', 
-        fullDesc: 'Pin chân phổ thông 12Ah\n Phân loại: 10 Cell 21700, báo pin phía trước\n Lõi Pin: EVE40P, Vtc6A, Samsung40T, LG50LT, Samsung50E...\n Mạch bảo vệ: BW-60A, BM-90A, DX-100A\n Chất liệu: Nhựa ABS\n Giá: 850-1tr\n Liên hệ để được tư vấn giá, chọn lõi pin và mạch phù hợp', 
+        fullDesc: 'Pin chân phổ thông 12Ah\n Phân loại: 15 Cell 21700, báo pin phía trước\n Lõi Pin: EVE40P, Vtc6A, Samsung40T, LG50LT, Samsung50E...\n Mạch bảo vệ: BW-60A, BM-90A, DX-100A\n Chất liệu: Nhựa ABS\n Giá: 850K-1tr\n Liên hệ để được tư vấn giá, chọn lõi pin và mạch phù hợp', 
         image: 'media/Pin_15cell_21700.jpg',
         // Thêm mảng này để chứa nhiều hình cho phần xem chi tiết
         images: [
@@ -178,7 +192,7 @@ const products = [
         ]
     },
     { 
-        id: 24, 
+        id: 25, 
         category: 'may-cong-cu', 
         name: 'Đóng pin theo yêu cầu', 
         fullDesc: 'Makita, Milwaukee, Bosch, DCA...', 
@@ -197,7 +211,7 @@ const products = [
     { 
         id: 30, 
         category: 'Acquy-Xe-May', 
-        name: 'Acquy Xe Máy', 
+        name: 'Acquy Xe Máy YT4 Mẫu 1', 
         fullDesc: 'Acquy Xe Máy YT4\n Kích thước (Dài x Rộng x Cao): Khoảng 113 mm x 70 mm x 85 mm. \n Phân loại:\n- Honda: Air Blade (đời cũ), Click, Future (từ năm 2008 trở đi), Wave (các dòng RS, RSV, 100s, ZX), Blade, Winner, Super Cub\n - Yamaha: Exciter, Nouvo (đời 1-5), Nozza, Acruzo, Grande, Janus, Sirius (sau 2010), Jupiter\n - Suzuki: Viva, Axelo, Address, Hayate.\n Lõi Pin: LFP 32700, LFP A123B\n Mạch bảo vệ: BW-QD600A\n Chất liệu: Nhựa ABS & Cọc bình mạ đồng\n Giá: 400-450K', 
         image: 'media/Acquy_YT4_1_1.jpg',
         // Thêm mảng này để chứa nhiều hình cho phần xem chi tiết
@@ -205,6 +219,72 @@ const products = [
            "media/Acquy_YT4_1_1.jpg",  
            "media/Acquy_YT4_1.jpg"
          
+         
+        ]
+    },
+    { 
+        id: 31, 
+        category: 'Acquy-Xe-May', 
+        name: 'Acquy Xe Máy YT4 Mẫu 2', 
+        fullDesc: 'Acquy Xe Máy YT4\n Kích thước (Dài x Rộng x Cao): Khoảng 113 mm x 70 mm x 85 mm. \n Phân loại:\n- Honda: Air Blade (đời cũ), Click, Future (từ năm 2008 trở đi), Wave (các dòng RS, RSV, 100s, ZX), Blade, Winner, Super Cub\n - Yamaha: Exciter, Nouvo (đời 1-5), Nozza, Acruzo, Grande, Janus, Sirius (sau 2010), Jupiter\n - Suzuki: Viva, Axelo, Address, Hayate.\n Lõi Pin: LFP 32700, LFP A123B\n Mạch bảo vệ: BW-QD600A\n Chất liệu: Nhựa ABS & Cọc bình mạ đồng\n Giá: 400-450K', 
+        image: 'media/Acquy_YT4_2_1.jpg',
+        // Thêm mảng này để chứa nhiều hình cho phần xem chi tiết
+        images: [
+           "media/Acquy_YT4_2_1.jpg",  
+           "media/Acquy_YT4_2.jpg"
+         
+         
+        ]
+    },
+     { 
+        id: 32, 
+        category: 'Acquy-Xe-May', 
+        name: 'Acquy Xe Máy YT4 Mẫu 3', 
+        fullDesc: 'Acquy Xe Máy YT4\n Kích thước (Dài x Rộng x Cao): Khoảng 113 mm x 70 mm x 85 mm. \n Phân loại:\n- Honda: Air Blade (đời cũ), Click, Future (từ năm 2008 trở đi), Wave (các dòng RS, RSV, 100s, ZX), Blade, Winner, Super Cub\n - Yamaha: Exciter, Nouvo (đời 1-5), Nozza, Acruzo, Grande, Janus, Sirius (sau 2010), Jupiter\n - Suzuki: Viva, Axelo, Address, Hayate.\n Lõi Pin: LFP 32700, LFP A123B\n Mạch bảo vệ: BW-QD600A\n Chất liệu: Nhựa ABS & Cọc bình mạ đồng\n Giá: 400-450K', 
+        image: 'media/Acquy_YT4_3.jpg',
+        // Thêm mảng này để chứa nhiều hình cho phần xem chi tiết
+        images: [
+           "media/Acquy_YT4_3.jpg",  
+                  
+        ]
+    },
+    { 
+        id: 33, 
+        category: 'Acquy-Xe-May', 
+        name: 'Acquy Xe Máy YT5 Mẫu 1', 
+        fullDesc: 'Acquy Xe Máy YT5\n Kích thước (Dài x Rộng x Cao): Khoảng 113 mm x 70 mm x 105 mm. \n Phân loại:\n- Honda: Air Blade (đời cũ), Click, Future (từ năm 2008 trở đi), Wave (các dòng RS, RSV, 100s, ZX), Blade, Winner, Super Cub\n - Yamaha: Exciter, Nouvo (đời 1-5), Nozza, Acruzo, Grande, Janus, Sirius (sau 2010), Jupiter\n - Suzuki: Viva, Axelo, Address, Hayate.\n Lõi Pin: LFP 32700, LFP A123B\n Mạch bảo vệ: BW-QD600A\n Chất liệu: Nhựa ABS & Cọc bình mạ đồng\n Giá: 400-550K', 
+        image: 'media/Acquy_YT5_1_2.jpg',
+        // Thêm mảng này để chứa nhiều hình cho phần xem chi tiết
+        images: [
+           "media/Acquy_YT5_1_2.jpg",  
+           "media/Acquy_YT5_1.jpg"
+         
+         
+        ]
+    },
+    { 
+        id: 34, 
+        category: 'Acquy-Xe-May', 
+        name: 'Acquy Xe Máy YT5 Mẫu 2', 
+        fullDesc: 'Acquy Xe Máy YT5\n Kích thước (Dài x Rộng x Cao): Khoảng 113 mm x 70 mm x 105 mm. \n Phân loại:\n- Honda: Air Blade 125cc (các đời có hệ thống đề từ / Idling Stop), Lead 125cc (đời dùng đề từ / Lead mới), SH Mode 125cc, SH 125i / 150i (các đời sử dụng đề từ), PCX 125cc, Vision (một số đời hoặc tùy phiên bản hộc bình), Click Thái / Vario\n - Yamaha: Nozza 115cc / Nozza Grande 125cc, Janus\n - Suzuki: Impulse 125cc (dòng xe tay ga phổ thông của Suzuki)\n Lõi Pin: LFP 32700, LFP A123B\n Mạch bảo vệ: BW-QD600A\n Chất liệu: Nhựa ABS & Cọc bình mạ đồng\n Giá: 400-550K', 
+        image: 'media/Acquy_YT5_2_2.jpg',
+        // Thêm mảng này để chứa nhiều hình cho phần xem chi tiết
+        images: [
+           "media/Acquy_YT5_2_2.jpg",  
+           "media/Acquy_YT5_2_1.jpg",
+           "media/Acquy_YT5_2.jpg"
+        ]
+    },
+     { 
+        id: 35, 
+        category: 'Acquy-Xe-May', 
+        name: 'Acquy Xe Máy YT5 Mẫu 3', 
+        fullDesc: 'Acquy Xe Máy YT5\n Kích thước (Dài x Rộng x Cao): Khoảng 113 mm x 70 mm x 105 mm. \n Phân loại:\n- Honda: Air Blade 125cc (các đời có hệ thống đề từ / Idling Stop), Lead 125cc (đời dùng đề từ / Lead mới), SH Mode 125cc, SH 125i / 150i (các đời sử dụng đề từ), PCX 125cc, Vision (một số đời hoặc tùy phiên bản hộc bình), Click Thái / Vario\n - Yamaha: Nozza 115cc / Nozza Grande 125cc, Janus\n - Suzuki: Impulse 125cc (dòng xe tay ga phổ thông của Suzuki)\n Lõi Pin: LFP 32700, LFP A123B\n Mạch bảo vệ: BW-QD600A\n Chất liệu: Nhựa ABS & Cọc bình mạ đồng\n Giá: 400-550K', 
+        image: 'media/Acquy_YT5_3_2.jpg',
+        // Thêm mảng này để chứa nhiều hình cho phần xem chi tiết
+        images: [
+           "media/Acquy_YT5_3_2.jpg",  
+           "media/Acquy_YT5_3_1.jpg"
          
         ]
     },
